9baa422a027dcafeab9a4594eb14e4f47f30c135
julie <julie@DESKTOP-TPMI0MS.lan>
2025-08-27 23:30:28 -0400

    added name in comment.

--- LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,7 +1,10 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+	/**
+	 * Julie-Anne Lewis
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World AGAIN!!");
 
